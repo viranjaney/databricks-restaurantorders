b83104b2b38d6bedfb21a334d1956f0ec56713a6
Viranjaney Gudaganavar <viranjneyg176@gmail.com>
2026-03-01 22:19:29 +0000

    Refactor fact_order_items to use pipelines

--- pipeline_transform_silver/fact_order_items.py
+++ pipeline_transform_silver/fact_order_items.py
@@ -1,54 +1,53 @@
-# Databricks notebook source
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from pyspark import pipelines as dp
 
-# COMMAND ----------
 
-df = (
-    spark.read.table("01_bronze.orders")
+@dp.table(name="02_silver.fact_order_items", table_properties={"quality":"silver"})
+@dp.expect_all_or_drop(
+    {
+        "valid_order_id" : "order_id IS NOT NULL",
+        "valid_item_id" : "item_id IS NOT NULL",
+        "valid_restaurant_id" : "restaurant_id IS NOT NULL",
+        "valid_order_timestamp" : "order_timestamp IS NOT NULL",
+        "valid_quantity" : "quantity > 0",
+        "valid_unit_price" : "unit_price > 0",
+        "valid_subtotal" : "subtotal > 0"
+    }
 )
+def fact_order_items():
+    items_schema = ArrayType(
+            StructType(
+                [
+                    StructField("item_id", StringType()),
+                    StructField("name", StringType()),
+                    StructField("category", StringType()),
+                    StructField("quantity", IntegerType()),
+                    StructField("unit_price", DecimalType(10, 2)),
+                    StructField("subtotal", DecimalType(10, 2)),
+                ]
+            )
+        )
 
-# COMMAND ----------
 
-display(df.limit(1))
-
-# COMMAND ----------
-
-items_schema = ArrayType(
-        StructType(
-            [
-                StructField("item_id", StringType()),
-                StructField("name", StringType()),
-                StructField("category", StringType()),
-                StructField("quantity", IntegerType()),
-                StructField("unit_price", DecimalType(10, 2)),
-                StructField("subtotal", DecimalType(10, 2)),
-            ]
+    df_fact_orders = (
+        spark.table("01_bronze.orders")
+        .withColumn("order_timestamp", to_timestamp(col("timestamp")))
+        .withColumn("order_date", to_date("order_timestamp"))
+        .withColumn("items_parsed", from_json(col("items"), items_schema))
+        .withColumn("item", explode(col("items_parsed")))
+        .select(
+            "order_id",
+            col("item.item_id").alias("item_id"),
+            "restaurant_id",
+            "order_timestamp",
+            "order_date",
+            col("item.name").alias("item_name"),
+            col("item.category").alias("category"),
+            col("item.quantity").alias("quantity"),
+            col("item.unit_price").cast("decimal(10,2)").alias("unit_price"),
+            col("item.subtotal").cast("decimal(10,2)").alias("subtotal")
         )
     )
 
-
-df_fact_orders = (
-    spark.table("01_bronze.orders")
-    .withColumn("order_timestamp", to_timestamp(col("timestamp")))
-    .withColumn("order_date", to_date("order_timestamp"))
-    .withColumn("items_parsed", from_json(col("items"), items_schema))
-    .withColumn("item", explode(col("items_parsed")))
-    .select(
-        "order_id",
-        col("item.item_id").alias("item_id"),
-        "restaurant_id",
-        "order_timestamp",
-        "order_date",
-        col("item.name").alias("item_name"),
-        col("item.category").alias("category"),
-        col("item.quantity").alias("quantity"),
-        col("item.unit_price").cast("decimal(10,2)").alias("unit_price"),
-        col("item.subtotal").cast("decimal(10,2)").alias("subtotal")
-    )
-)
-
-display(df_fact_orders.limit(5))
-
-# COMMAND ----------
-
+    return df_fact_orders
